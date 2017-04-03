66a8e8cb0a3fe43caebeac97ce8e601dde14b2bc
vevedh <vevedh@gmail.com>
2017-04-03 00:02:00 +0000

    Premiers tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Accueil', component: Page1 },
+      { title: 'Systeme', component: Page2 }
     ];
 
   }
